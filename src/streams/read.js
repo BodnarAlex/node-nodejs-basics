f5cd618a7c536b9527be9ebce3f44cc5c38efa2e
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-28 20:21:16 +0200

    fix: change script for print equal data for read at streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -6,10 +6,12 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const pathRead = path.resolve(__dirname, "files", "fileToRead.txt");
 
 const read = async () => {
+    let data = "";
     const stream = fs.createReadStream(pathRead, "utf-8");
-    stream.on('data', chunk => {
-        console.log(chunk);
-    });
+
+    stream.on("data", (chunk) => (data += chunk));
+    stream.on("end", () => process.stdout.write(data));
+    stream.on("error", (error) => console.log("Error", error.message));
 };
 
 await read();
