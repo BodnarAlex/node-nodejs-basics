a725dd2b18b1840db0595a7a2d11c19d65626b16
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-28 20:15:02 +0200

    feat: add solution of read at streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathRead = path.resolve(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+    const stream = fs.createReadStream(pathRead, "utf-8");
+    stream.on('data', chunk => {
+        console.log(chunk);
+    });
 };
 
 await read();
