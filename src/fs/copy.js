2fddefdc42dd101a9c5103554b04fa82b64ba1c0
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-28 16:13:23 +0200

    feat: add solution of copy from fs

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
-const copy = async () => {
-    // Write your code here 
+import fs from "fs/promises";
+import path from "path";
+
+const __dirname = import.meta.dirname;
+const pathFrom = path.resolve(__dirname, "files");
+const pathTo = path.resolve(__dirname, "files_copy");
+
+const copy = async (pathFrom, pathTo) => {
+    try {
+        const files = await fs.readdir(pathFrom, { withFileTypes: true });
+        await fs.mkdir(pathTo);
+        for (const file of files) {
+            if (file.isFile())
+                await fs.copyFile(path.resolve(pathFrom, file.name), path.resolve(pathTo, file.name));
+            else
+                await copy(path.resolve(pathFrom, file.name), path.resolve(pathTo, file.name));
+        }
+    } catch {
+        throw new Error("FS operation failed");
+    }
 };
 
-await copy();
+await copy(pathFrom, pathTo);
