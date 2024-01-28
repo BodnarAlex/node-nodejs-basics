10481413b803b9b3ecb7889620c2371f11fa7f31
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-28 14:12:54 +0200

    feat: add solution of create from fs

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from "fs/promises";
+import path from "path";
+
+const __dirname = import.meta.dirname;
+const PathTo = path.resolve(__dirname, "files", "fresh.txt");
+
+const greeting = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+    try {
+        fs.writeFile(PathTo, greeting, {flag: "wx"});
+    } catch {
+        throw new Error ("FS operation failed");
+    }
 };
 
 await create();
