10c0ce64494545b339570cb9b0950aa191a4c3c8
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-29 16:05:44 +0200

    feat: add script for post worker data from worker at wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import os from "os";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { Worker } from "worker_threads";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathTo = path.resolve(__dirname, "worker.js");
+
 const performCalculations = async () => {
-    // Write your code here
+
+    let finalArray = [];
+    const ten = 10;
+    const cpuNumb = os.cpus().length;
+    console.log("cpuNumb: ", cpuNumb);
+
+    const worker = new Worker(pathTo, {
+        workerData: cpuNumb
+    });
+
+    worker.on("message", msg => {
+        finalArray.push({ status: "resolved", data: msg });
+        console.log(finalArray);
+    })
+    worker.on("error", err => {
+    finalArray.push({ status: "error", data: null });
+    console.log(finalArray);
+})
+console.log(finalArray);
 };
 
 await performCalculations();
