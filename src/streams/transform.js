5d6aec8f5f9987f7ad490f054103684cb9804e71
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-29 07:53:26 +0200

    feat: add solution of transform at streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { Transform, pipeline } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+    const transforming = new Transform({
+        transform(chunk, enc, cb) {
+            const reversre = chunk.toString().trim().split("").reverse().join("");
+            this.push(reversre + "\n");
+            cb();
+        }
+    })
+
+    pipeline(
+        process.stdin,
+        transforming,
+        process.stdout,
+        err => {
+            console.log(err)
+        });
 };
 
 await transform();
