400b7e075a539711c795ec004d22ee8542e68acd
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-29 08:33:56 +0200

    feat: add solution of parse arg at cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const newArr = Array.from({ length: 2 }, (item, i) =>
+        args.slice(i * 2, (i + 1) * 2)
+    );
+    const finalString = newArr.reduce((prev, [key, value]) => {
+        if (key.includes("--")) {
+            key = key.replace("--", "");
+            return `${prev}${key} is ${value}, `;
+        }
+    }, "")
+    console.log(finalString);
 };
 
 parseArgs();
