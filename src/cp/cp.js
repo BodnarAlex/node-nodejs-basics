4190e439883abf6edc737b8b7adb33718f8bcbb5
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-29 16:51:28 +0200

    feat: add solution of child process at cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import cp from "child_process";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathToCp = path.resolve(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    cp.fork(pathToCp, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ["someArgument1", "someArgument2", 8, 7, 6, 5, 4, 3, 2, 1, "Win"]);
