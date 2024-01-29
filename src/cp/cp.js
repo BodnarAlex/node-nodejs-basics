713df80290fc3d440b2f6a82839c968c1b870bce
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-29 21:27:03 +0200

    fix: rewrite function on spawn for child process at cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -6,7 +6,9 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const pathToCp = path.resolve(__dirname, "files", "script.js");
 
 const spawnChildProcess = async (args) => {
-    cp.fork(pathToCp, args);
+    const childProcess = cp.spawn('node', [pathToCp, ...args]);
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
