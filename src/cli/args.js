ecbb87e025688dec5e0fe752d6f47482d5ab8e75
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-29 08:52:40 +0200

    fix: add check rare situations for solution of parse arg at cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,9 +2,11 @@ const parseArgs = () => {
     const args = process.argv.slice(2);
     let finalString = "";
     for (let i = 0; i < args.length; i++) {
-        if (args[i].startsWith("--")) {
-            finalString = `${finalString}${args[i].slice(2)} is ${args[i + 1]}, `;
-            i++;
+        if (i + 1 < args.length) {
+            if (args[i].startsWith("--") && !args[i + 1].startsWith("--")) {
+                finalString = `${finalString}${args[i].slice(2)} is ${args[i + 1]}, `;
+                i++;
+            }
         }
     }
     console.log(finalString.slice(0, -2));
