2756e10e30ae6c87854d7ce22dbdb2882645bd9b
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-29 09:37:41 +0200

    feat: add solution of parse env at cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    let finalString = "";
+    let env = process.env;
+    const keys = Object.keys(env).filter((x) => x.startsWith("RSS_"));
+
+    for(let keyValue of keys){
+        finalString += `${keyValue}=${env[keyValue]}; `;
+    }
+    console.log(finalString.slice(0, -2));
 };
 
 parseEnv();
