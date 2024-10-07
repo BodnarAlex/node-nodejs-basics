de482f6510df5ae03d316543039b7815ceba0a9b
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-10-08 01:02:29 +0200

    refactor: correct rename of variable

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -3,8 +3,8 @@ import { Transform, pipeline } from "stream";
 const transform = async () => {
     const transforming = new Transform({
         transform(chunk, enc, cb) {
-            const reversre = chunk.toString().trim().split("").reverse().join("");
-            this.push(reversre + "\n");
+            const reverse = chunk.toString().trim().split("").reverse().join("");
+            this.push(reverse + "\n");
             cb();
         }
     })
