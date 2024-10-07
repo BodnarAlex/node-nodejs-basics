bb39ba2ab447ae44b945ca87520da60d7cc05a13
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-10-08 01:22:13 +0200

    fix: add extra checks for accesses

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -8,6 +8,15 @@ const pathTo = path.resolve(__dirname, "files_copy");
 
 const copy = async () => {
     try {
+        await fs.access(pathFrom);
+        try {
+            await fs.access(pathTo);
+            throw new Error("FS operation failed");
+        } catch (err) {
+            if (err.code !== 'ENOENT') {
+                throw new Error("FS operation failed");
+            }
+        }
         await fs.cp(pathFrom, pathTo, { recursive: true, force: false, errorOnExist: true });
     } catch (error) {
         throw new Error("FS operation failed");
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -8,6 +8,15 @@ const pathNew = path.resolve(__dirname, "files", "properFilename.md");
 
 const rename = async () => {
     try {
+        await fs.access(pathOld);
+        try {
+            await fs.access(pathNew);
+            throw new Error("FS operation failed");
+        } catch (err) {
+            if (err.code !== 'ENOENT') {
+                throw new Error("FS operation failed");
+            }
+        }
         await fs.rename(pathOld, pathNew);
     } catch {
         throw new Error("FS operation failed");
