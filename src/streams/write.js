d116f57239cd251585634040e722fc685ba8d5d7
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2025-04-28 16:25:07 +0200

    feat: add solution of write at streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import { createWriteStream } from 'fs';
+import path from "path";
+
+const __dirname = import.meta.dirname;
+const pathToFile = path.resolve(__dirname, "files", 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const stream = createWriteStream(pathToFile, { encoding: 'utf8' });
+    process.stdin.pipe(stream);
 };
 
 await write();
