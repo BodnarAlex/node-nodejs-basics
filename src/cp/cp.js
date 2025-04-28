3d7567850f591c6102fb1acf9c46fdd9e973a264
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2025-04-29 00:59:13 +0200

    fix: add error check for cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -6,8 +6,12 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const pathToCp = path.resolve(__dirname, "files", "script.js");
 
 const spawnChildProcess = async (args) => {
-    fork(pathToCp, args, {
+    const child = fork(pathToCp, args, {
         stdio: [process.stdin, process.stdout, process.stderr, 'ipc']
+    });
+
+    child.on("error", (err) => {
+        throw new Error(err);
     });
 };
 
