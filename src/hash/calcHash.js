9a20a0d5993cd1ae07f4bd1a17ff59a9235782d0
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2025-04-28 17:35:34 +0200

    feat: add solution of calchash at hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import path from "path";
+import { createHash } from "crypto";
+import { fileURLToPath } from "url";
+import { createReadStream } from "fs";
+import { pipeline } from "stream/promises";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathRead = path.resolve(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+    const stream = createReadStream(pathRead);
+    const hash = createHash("sha256");
+
+    try {
+        await pipeline(stream, hash);
+        console.log(hash.digest("hex"));
+    } catch (err) {
+        throw new Error(err.message);
+    }
 };
 
 await calculateHash();
