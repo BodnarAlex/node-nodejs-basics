6b104c1bf1a09f51ab4e11a95bddb9b877438d32
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2025-04-28 02:05:20 +0200

    feat: add solution of copy from fs

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import { cp, access } from 'fs/promises';
+import path from "path";
+const errorText = 'FS operation failed';
+
+const __dirname = import.meta.dirname;
+const pathToCopyCatalog = path.resolve(__dirname, "files");
+const pathToPasteCatalog = path.resolve(__dirname, "files_copy");
+
 const copy = async () => {
-    // Write your code here 
+
+    try {
+        await access(pathToPasteCatalog);
+        throw new Error(errorText);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            try {
+                await cp(pathToCopyCatalog, pathToPasteCatalog, { recursive: true });
+                console.log('Files copied successfully');
+            } catch (copyError) {
+                throw new Error(errorText);
+            }
+        } else {
+            throw new Error(errorText);
+        }
+    }
 };
 
 await copy();
