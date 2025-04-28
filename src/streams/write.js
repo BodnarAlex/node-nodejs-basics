31e759923620636b59f3dc72a435bc16cfa4abf5
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2025-04-29 00:38:19 +0200

    fix: add check errors for wtite at stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -6,6 +6,15 @@ const pathToFile = path.resolve(__dirname, "files", 'fileToWrite.txt');
 
 const write = async () => {
     const stream = createWriteStream(pathToFile, { encoding: 'utf8' });
+
+    stream.on('error', (err) => {
+        console.error(err.message);
+    });
+
+    process.stdin.on('error', (err) => {
+        console.error(err.message);
+    });
+
     process.stdin.pipe(stream);
 };
 
